792c48b3a2f4a2a1f36070ec04c0775b85bf3e21
mathieu.dique <mathieu.dique@epita.fr>
2022-11-02 04:34:42 +0530

    Refactor
    - Added a struct to contains all the variables related to the scraping
    - Added methods

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,59 @@ use scraper::{Html, Selector};
 pub mod backend;
 
 const RANKING_TABLE: usize = 2;
+const MATCH_TABLE: usize = 3;
+
+struct Worker<'a> {
+    tr: Selector,
+    td: Selector,
+    table: Selector,
+    uri: &'a str,
+    document: Html,
+}
+
+impl<'a> Worker<'a> {
+    fn new(uri: &str) -> Worker {
+        Worker {
+            table: Selector::parse("table").unwrap(),
+            tr: Selector::parse("tr").unwrap(),
+            td: Selector::parse("td").unwrap(),
+            uri,
+            document: Html::parse_document(&backend::get_uri_html_content(uri)[..]),
+        }
+    }
+
+    fn scrap_ranks(&self) {
+        let ranking_table = self
+            .document
+            .select(&self.table)
+            .nth(RANKING_TABLE)
+            .unwrap();
+
+        // println!("{}", table_content.inner_html());
+        let mut ranking_table_lines = ranking_table.select(&self.tr);
+        let _empty_line = ranking_table_lines.next();
+
+        for ranking_table_line in ranking_table_lines {
+            let mut team_line = ranking_table_line.select(&self.td);
+
+            let team_rank: usize = team_line.next().unwrap().inner_html()[..1]
+                .parse::<usize>()
+                .unwrap();
+
+            let team_name: String = team_line.next().unwrap().inner_html();
+
+            println!("{team_rank} {team_name}");
+        }
+    }
+}
 
 fn main() {
     let ffvb_uri: &str = "https://www.ffvbbeach.org/ffvbapp/resu/vbspo_calendrier.php?saison=2022/2023&codent=LIIDF&poule=RMA";
+    /*
+    let selectors: Selectors =
     let content: String = backend::get_uri_html_content(ffvb_uri);
-    // println!("{}", content);
     let html: Html = Html::parse_document(&content);
-    let table_selector: Selector = Selector::parse("table").unwrap();
-    let tr_selector: Selector = Selector::parse("tr").unwrap();
-    let td_selector: Selector = Selector::parse("td").unwrap();
-
-    let ranking_table = html.select(&table_selector).nth(RANKING_TABLE).unwrap();
-
-    // println!("{}", table_content.inner_html());
-    let mut ranking_table_lines = ranking_table.select(&tr_selector);
-    let _empty_line = ranking_table_lines.next();
-
-    for ranking_table_line in ranking_table_lines {
-        let mut team_line = ranking_table_line.select(&td_selector);
-
-        let team_rank: usize = team_line.next().unwrap().inner_html()[..1]
-            .parse::<usize>()
-            .unwrap();
-
-        let team_name: String = team_line.next().unwrap().inner_html();
-
-        println!("{team_rank} {team_name}");
-    }
+    */
+    let worker = Worker::new(ffvb_uri);
+    worker.scrap_ranks();
 }
