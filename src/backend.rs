49b7e7df513128b050c220e482a7a4e539b38c93
mthdqe <dique.mathieu@gmail.com>
2022-11-14 19:37:43 +0100

    Refactor
    - Moved the worker struct in backend
    - Cleaned the main

--- src/backend.rs
+++ src/backend.rs
@@ -1,24 +1,15 @@
 use reqwest::blocking::*;
+use scraper::{Html, Selector};
 
-/*
-use reqwest::tls::Identity;
+const RANKING_TABLE: usize = 2;
+const MATCH_TABLE: usize = 3;
 
-use std::fs::File;
-use std::io::Read;
-*/
-
-pub fn get_uri_html_content(uri: &str) -> String {
-    /* Build the pem buffer (Contains the x509 certificate and key) */
-    /*
-    let mut pem_buf: Vec<u8> = Vec::new();
-    File::open("/home/dev/certif/certif.pem")
-        .unwrap()
-        .read_to_end(&mut pem_buf)
-        .unwrap();
-
-    /* Build the x509 identity */
-    let identity: Identity = Identity::from_pem(&pem_buf).unwrap();
-    */
+/**
+ * @brief Gets the html content of a web page as a string
+ * @param uri The uri from which gets the web page content
+ * @return The web page as a text (string)
+ */
+fn get_uri_html_content(uri: &str) -> String {
     /* Create the client builder */
     let client_builder: ClientBuilder = ClientBuilder::new().danger_accept_invalid_certs(true);
 
@@ -27,4 +18,90 @@ pub fn get_uri_html_content(uri: &str) -> String {
 
     /* Sends the get request and returns the html content as a String */
     client.get(uri).send().unwrap().text().unwrap()
+}
+
+/**
+ * @brief Structure that holds elements to scrap a website
+ */
+pub struct Worker<'a> {
+    tr: Selector,
+    td: Selector,
+    table: Selector,
+    uri: &'a str,
+    document: Html,
+}
+
+impl<'a> Worker<'a> {
+    /**
+     * @brief Create a new Worker
+     * @param uri The uri to create a worker from
+     * @return Worker
+     */
+    pub fn new(uri: &str) -> Worker {
+        Worker {
+            table: Selector::parse("table").unwrap(),
+            tr: Selector::parse("tr").unwrap(),
+            td: Selector::parse("td").unwrap(),
+            uri,
+            document: Html::parse_document(&get_uri_html_content(uri)[..]),
+        }
+    }
+
+    /**
+     * @brief Scrap the ranks
+     */
+    pub fn scrap_ranks(&self) {
+        let ranking_table = self
+            .document
+            .select(&self.table)
+            .nth(RANKING_TABLE)
+            .unwrap();
+
+        // println!("{}", table_content.inner_html());
+        let mut ranking_table_lines = ranking_table.select(&self.tr);
+        let _empty_line = ranking_table_lines.next();
+
+        for ranking_table_line in ranking_table_lines {
+            let mut team_line = ranking_table_line.select(&self.td);
+
+            let team_rank: usize = team_line.next().unwrap().inner_html()[..1]
+                .parse::<usize>()
+                .unwrap();
+
+            let team_name: String = team_line.next().unwrap().inner_html();
+
+            println!("{team_rank} {team_name}");
+        }
+    }
+
+    /**
+     * @brief Scrap the matchs of a team
+     * @param team_name The team to get the match from
+     */
+    pub fn scrap_matchs(&self, team_name: &str) {
+        let match_table = self.document.select(&self.table).nth(MATCH_TABLE).unwrap();
+        let match_lines = match_table.select(&self.tr);
+
+        for match_line in match_lines {
+            let line_elts: Vec<scraper::ElementRef> = match_line.select(&self.td).collect();
+
+            if line_elts.len() > 1 {
+                let date = line_elts[1].inner_html();
+                let hour = line_elts[2].inner_html();
+                let team_1 = line_elts[3].inner_html();
+                let team_2 = line_elts[5].inner_html();
+                let place = line_elts[7].inner_html();
+
+                if (team_1 == team_name || team_2 == team_name)
+                    && (team_1 != "xxxxx" && team_2 != "xxxxx")
+                {
+                    println!("[{team_1} VS {team_2}]");
+                    println!("Date: {date}");
+                    println!("Heure: {hour}");
+                    println!("Lieu: {place}");
+                    println!("\n");
+                }
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,11 @@
-use scraper::{Html, Selector};
 pub mod backend;
-
-const RANKING_TABLE: usize = 2;
-const MATCH_TABLE: usize = 3;
-
-struct Worker<'a> {
-    tr: Selector,
-    td: Selector,
-    table: Selector,
-    uri: &'a str,
-    document: Html,
-}
-
-impl<'a> Worker<'a> {
-    fn new(uri: &str) -> Worker {
-        Worker {
-            table: Selector::parse("table").unwrap(),
-            tr: Selector::parse("tr").unwrap(),
-            td: Selector::parse("td").unwrap(),
-            uri,
-            document: Html::parse_document(&backend::get_uri_html_content(uri)[..]),
-        }
-    }
-
-    fn scrap_ranks(&self) {
-        let ranking_table = self
-            .document
-            .select(&self.table)
-            .nth(RANKING_TABLE)
-            .unwrap();
-
-        // println!("{}", table_content.inner_html());
-        let mut ranking_table_lines = ranking_table.select(&self.tr);
-        let _empty_line = ranking_table_lines.next();
-
-        for ranking_table_line in ranking_table_lines {
-            let mut team_line = ranking_table_line.select(&self.td);
-
-            let team_rank: usize = team_line.next().unwrap().inner_html()[..1]
-                .parse::<usize>()
-                .unwrap();
-
-            let team_name: String = team_line.next().unwrap().inner_html();
-
-            println!("{team_rank} {team_name}");
-        }
-    }
-
-    fn scrap_matchs(&self, team_name: &str) {
-        let match_table = self.document.select(&self.table).nth(MATCH_TABLE).unwrap();
-        let match_lines = match_table.select(&self.tr);
-
-        for match_line in match_lines {
-            let line_elts: Vec<scraper::ElementRef> = match_line.select(&self.td).collect();
-
-            if line_elts.len() > 1 {
-                let date = line_elts[1].inner_html();
-                let hour = line_elts[2].inner_html();
-                let team_1 = line_elts[3].inner_html();
-                let team_2 = line_elts[5].inner_html();
-                let place = line_elts[7].inner_html();
-
-                if (team_1 == team_name || team_2 == team_name)
-                    && (team_1 != "xxxxx" && team_2 != "xxxxx")
-                {
-                    println!("[{team_1} VS {team_2}]");
-                    println!("Date: {date}");
-                    println!("Heure: {hour}");
-                    println!("Lieu: {place}");
-                    println!("\n");
-                }
-            }
-        }
-    }
-}
+use backend::Worker;
 
 fn main() {
     let ffvb_uri: &str = "https://www.ffvbbeach.org/ffvbapp/resu/vbspo_calendrier.php?saison=2022/2023&codent=LIIDF&poule=RMA";
-    /*
-    let selectors: Selectors =
-    let content: String = backend::get_uri_html_content(ffvb_uri);
-    let html: Html = Html::parse_document(&content);
-    */
+
     let worker = Worker::new(ffvb_uri);
+
     worker.scrap_ranks();
     worker.scrap_matchs("C S M CLAMART 2");
     worker.scrap_matchs("UGS DRAVEIL JUVISY ATHIS-MONS VOLLEY-BALL");
